24803a4ddb6e9e498aa3f4dadeb82d14a8b6a8fe
Andrew Etringer <aetringer@epri.com>
2020-10-22 10:49:58 -0700

    WIP adds chiller technology

--- MicrogridDER/Chiller.py
+++ MicrogridDER/Chiller.py
@@ -42,6 +42,8 @@ class Chiller(DER, ContinuousSizing, DERExtension):
         ContinuousSizing.__init__(self, params)
         DERExtension.__init__(self, params)
 
+        KW_PER_TON = 3.5168525  # unit conversion (1 ton in kW)
+
         self.technology_type = 'thermal'
         self.tag = 'Chiller'
 
@@ -50,14 +52,14 @@ class Chiller(DER, ContinuousSizing, DERExtension):
         self.cop = params['coefficient_of_performance']
         self.power_source = params['power_source']  # electricity, natural_gas, heat
 
-        self.rated_capacity = params['rated_capacity']  # tons/chiller
+        self.rated_power = KW_PER_TON * params['rated_capacity']  # tons/chiller
 
         self.ccost = params['ccost']  # $/chiller
-        self.ccost_ton = params['ccost_ton']  # $/tons-chiller
+        self.ccost_ton = params['ccost_ton'] / KW_PER_TON  # $/tons-chiller
         #self.capital_cost_function = [params['ccost'],
         #                              params['ccost_ton']]
 
-        self.fixed_om_cost = params['fixed_om_cost']  # $ / ton-year
+        self.fixed_om_cost = params['fixed_om_cost'] / KW_PER_TON  # $ / ton-year
 
         self.n = params['n']  # number of chillers (integer)
 
@@ -67,6 +69,15 @@ class Chiller(DER, ContinuousSizing, DERExtension):
         # time series inputs
         self.site_cooling_load = params.get('site_cooling_load')    # BTU/hr
 
+        self.max_rated_power = KW_PER_TON * params['max_rated_capacity']  # tons/chiller
+        self.min_rated_power = KW_PER_TON * params['min_rated_capacity'] # tons/chiller
+        if not self.rated_power:
+            self.rated_power = cvx.Variable(integer=True, name=f'{self.name}rating')
+            self.size_constraints += [cvx.NonPos(-self.rated_power)]
+            if self.min_rated_power:
+                self.size_constraints += [cvx.NonPos(self.min_rated_power - self.rated_power)]
+            if self.max_rated_power:
+                self.size_constraints += [cvx.NonPos(self.rated_power - self.max_rated_power)]
 
     def grow_drop_data(self, years, frequency, load_growth):
         if self.site_cooling_load is not None:
@@ -75,19 +86,39 @@ class Chiller(DER, ContinuousSizing, DERExtension):
             self.site_cooling_load = Lib.drop_extra_data(self.site_cooling_load, years)
 
     def initialize_variables(self, size):
-    #    # rotating generation
-    #    super().initialize_variables(size)
-        # plus cooling
-        self.variables_dict.update({
+        # TODO -- add rated_capacity sizing optimization variable here, when sizing
+        self.variables_dict = {
             'cold': cvx.Variable(shape=size, name=f'{self.name}-coldP', nonneg=True),
-        })
+        }
 
     #def discharge_capacity(self, solution=False):
     #def name_plate_capacity(self, solution=False):
 
 
-    #def constraints(self, mask, **kwargs):
-    #def objective_function(self, mask, annuity_scalar=1):
+    def constraints(self, mask, **kwargs):
+        constraint_list = super().constraints(mask)
+        cold = self.variables_dict['cold']
+
+        if self.power_source == 'electricity':
+            # TODO -- add the additional electricl load
+            constraint_list += [cvx.Zero(cold / self.cop)]
+
+        elif self.power_source == 'natural_gas':
+            # TODO -- add the additional NG use
+            constraint_list += [cvx.Zero(cold / self.cop)]
+
+        elif self.power_source == 'heat':
+            # TODO -- add the additional heat load
+            constraint_list += [cvx.Zero(cold / self.cop)]
+
+        constraint_list += self.size_constraints
+        return constaint_list
+
+    def objective_function(self, mask, annuity_scalar=1):
+        costs = super().objective_function(mask, annuity_scalar)
+        costs.update(self.sizing_objective())
+        return costs
+
     #def update_for_evaluation(self, input_dict):
 
     #def set_size(self):
